cee2289410b2f8239d86286babacafe1ccbf06aa
petrov-anna <anna.petrov-petrov@yandex.ru>
2021-03-09 09:17:41 +0300

    fifth task

--- myserver.py
+++ myserver.py
@@ -1,19 +1,24 @@
 import socket
+import logging
 from validator import port_validation
 
 DEFAULT_PORT = 9090
 
+# Настройки логирования
+logging.basicConfig(filename='server.log', filemode='w',
+                    format="%(asctime)-15s [%(levelname)s] %(funcName)s: %(message)s", level=logging.INFO)
+logger = logging.getLogger(__name__)
+
 
 class Server:
-
     def __init__(self, port_number: int) -> None:
         sock = socket.socket()
-        sock.bind(('', port_number))
+        sock.bind(("", port_number))
         sock.listen(0)
         # Наш сокет
         self.sock = sock
         # Текущее соединение
-        print(f"Сервер инициализировался, слушает порт {port_number}")
+        logging.info(f"Сервер инициализировался, слушает порт {port_number}")
         # Ожидаем новое подключение
         while True:
             conn, addr = self.sock.accept()
@@ -23,19 +28,20 @@ class Server:
         """
         Обработчик нового соединения
         """
-        print(f"Новое соединение от {addr}")
-        msg = ''
+        logging.info(f"Новое соединение от {addr}")
+        msg = ""
+
         while True:
             # Получаем данные
             data = conn.recv(1024)
-
             # Если нет данных - больше ничего не ожидаем от клиента
             if not data:
                 break
             msg += data.decode()
             conn.send(data)
-            data_str = str(data, 'utf-8')
-            print(f"Получили сообщение от клиента: '{data_str}'")
+
+            data_str = str(data, "utf-8")
+            logging.info(f"Получили сообщение от клиента: '{data_str}'")
 
 
 def main():
@@ -44,7 +50,6 @@ def main():
     if not port_flag:
         port_input = DEFAULT_PORT
         print(f"Выставили порт {port_input} по умолчанию")
-
     server = Server(int(port_input))
 
 
